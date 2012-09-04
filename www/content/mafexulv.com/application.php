2c7dbbd7e1ed48a8c87fd31b2bae476138e993f0
Nick <selling_tokyo@yahoo.com>
2012-09-04 19:41:09 -0400

    updated some Mafex content, added a todo
    yo no hablo the spanish

--- www/content/mafexulv.com/application.php
+++ www/content/mafexulv.com/application.php
@@ -19,14 +19,14 @@
 			?>
 
 			<h1>Areas of application</h1>
-			<a href="<?php echo PICTURES_LOCATION; ?>application1.jpg" rel="lightbox[application]" title="In between gravestones"><img class="pright" src="<?php echo PICTURES_LOCATION; ?>thumbs/application1.jpg" alt=""></a>
+			<a href="<?php echo PICTURES_LOCATION; ?>applicationpotato.jpg" rel="lightbox[mafexapplication]" title="MAFEX applicator used in potatoes."><img class="pright" src="<?php echo THUMBS_LOCATION; ?>applicationpotato.jpg" alt=""></a>
 
 			<h2>Potato Industry</h2>
 			<p>The MAFEX Ultra-Low Volume applicator can be used to apply any liquid. Its most common usage is for the liquid treatment of potatoes with seed dressing agents, fungicides and anti-sprouting agents (e.g. Sprout Nip® EC).</p>
 			 
 			<h2>Other Fruits</h2>
 			<p> The MAFEX can be used for the treatment of citrus and other fruits with various types of wax and fungicides. </p>
-			  <a href="<?php echo PICTURES_LOCATION; ?>application2.jpg" rel="lightbox[application]" title="In a tree nursery"><img class="pright" src="<?php echo PICTURES_LOCATION; ?>thumbs/application2.jpg" alt=""></a>
+			  <a href="<?php echo PICTURES_LOCATION; ?>applicationcitrus.jpg" rel="lightbox[mafexapplication]" title="MAFEX applicator used in citrus to add lustre."><img class="pright" src="<?php echo THUMBS_LOCATION; ?>applicationcitrus.jpg" alt=""></a>
 			<h2>Humidification</h2>
 			<p>Apply water with MAFEX for dust binding and humidity control.</p>
 
---
+++ www/content/mafexulv.com/home.php
@@ -0,0 +1,89 @@
+ <div class="padContent">
+
+ <?php
+
+   switch ($mankarMain->lang) {
+      
+      case LANGUAGE_SPANISH:
+        ?>
+
+          <h1>MAFEX® -Pulverizador fino ULV para esparcir productos fitosanitarios líquidos</h1> 
+          <a href="<?php echo PICTURES_LOCATION; ?>mafex_atomizer_benefit.jpg" rel="lightbox[mafexbenefits]" title="MAFEX® -Pulverizador fino ULV"><img class="pright" src="<?php echo THUMBS_LOCATION; ?>mafex_atomizer_benefit.jpg" alt=""></a>
+          <p>El pulverizador fino MAFEX-Potato y -Fruit se basa en la técnica ULV (Ultra Low Volume), mediante la cual se puede reducir notablemente la cantidad necesaria de líquido para esparcir, debido a una distribución óptima gracias al tamaño uniforme de las gotas. Se utiliza desde hace años especialmente para proteger de forma óptima la cosecha tanto de patatas con germinación y fungicidas, como de frutas con cera y fungicidas.</p>
+
+
+          <h2>Modo de función del procedimiento MAFEX</h2>
+          <a href="<?php echo PICTURES_LOCATION; ?>mafex_portable_benefit.jpg" rel="lightbox[mafexbenefits]" title="MAFEX® -Pulverizador fino ULV"><img class="pleft" src="<?php echo THUMBS_LOCATION; ?>mafex_portable_benefit.jpg" alt=""></a>  
+          <p>Para combatir los agentes dañinos es indispensable humedecer toda la superficie del tubérculo. Todas las partículas activas deben cubrir la superficie manteniendo entre ellas la mínima distancia posible. Para lograr esta distribución uniforme se debe esparcir la cantidad disponible del producto en numerosas gotas de tamaño uniforme.</p>
+          <p>Con el aparato MAFEX se producen aprox. 30millones de gotas a partir 1 ml de líquido gracias a la fuerza centrífuga producida por un disco de rotación especialmente desarrollado. Las gotas se almacenan uniformemente en la superficie debido a la corriente de aire vertical producida por el disco de rotación, a la fuerza de la gravedad y al movimiento de rotación y caída de la patata o fruta. Así se obtiene un rendimiento óptimo.</p>
+          <p>Unamoderna unidad de control permite el cebado automático del sistema, es decir, el control de todas las funciones y el transporte del líquido a la boquilla antes de que comience la pulverización. Se puede regular la cantidad deseada desde la unidad de control. Durante el funcionamiento, la función de la boquilla se encuentra vigilada y en caso de avería se activa una función de alarma. El control de la circulación es opcional.</p>
+
+      <?php
+      break;
+    case LANGUAGE_FRENCH:
+      ?>
+        <h1>Pulvérisateurs ULV fins MAFEX® - Pour l‘application de produits phytosanitaires liquides</h1> 
+        <a href="<?php echo PICTURES_LOCATION; ?>mafex_atomizer_benefit.jpg" rel="lightbox[mafexbenefits]" title="MAFEX disque rotatif"><img class="pright" src="<?php echo THUMBS_LOCATION; ?>mafex_atomizer_benefit.jpg" alt=""></a>
+        <p>Le pulvérisateur fin MAFEX Potato &#38; Fruit utilise la technique ULV (Ultra Low Volume) permettant de nettement réduire les quantité épandue grâce à une répartition optimale des agents actifs et des gouttelettes de taille régulière.  Celui-ci est utilisé depuis plusieurs années, tout particulièrement pour une protection optimale des récoltes de pommes de terre &#8211; avec des inhibiteurs de germination et des fongicides -, mais aussi de fruits &#8211; avec de la cire et des fongicides.</p>
+
+        <h2>Fonctionnement du pulvérisateur MAFEX® </h2>
+        <a href="<?php echo PICTURES_LOCATION; ?>mafex_portable_benefit.jpg" rel="lightbox[mafexbenefits]" title="Pulvérisateurs ULV fins MAFEX®"><img class="pleft" src="<?php echo THUMBS_LOCATION; ?>mafex_portable_benefit.jpg" alt=""></a>  
+        <p>L’imprégnation complète de la surface du tubercule est indispensable pour lutter contre les nuisibles. Les différentes particules de produit doivent recouvrir la surface en laissant le moins de vide possible. Afin d’obtenir une répartition la plus homogène possible, la quantité de préparation disponible doit être pulvérisée en une multitude des gouttelettes de taille homogène.</p>
+        <p>Sur les pulvérisateurs MAFEX®, la force centrifuge, associée à un disque rotatif spécialement développé, permet de générer quelque 30 millions de gouttelettes avec 1 ml de liquide. Ces gouttelettes se déposent régulièrement à la surface grâce à l’écoulement vertical généré par le disque rotatif, à la force de gravité, ainsi qu’aux mouvements de rotation et de chute des pommes de terre ou des fruits. Pour une efficacité parfaite.</p>
+        <p>Une unité de commande moderne permet le Priming automatique du système ; en d&#8217;autres termes, celle-ci contrôle toutes les fonctions, mais aussi l&#8217;alimentation du liquide vers la buse avec le début de l‘application. La quantité souhaitée peut être réglée sur l&#8217;unité de commande. Pendant l&#8217;utilisation, celle-ci contrôle le fonctionnement de la buse et déclenche une fonction d&#8217;alarme en cas de défaillance. Un contrôle du débit est également proposé en option.</p>
+        <br />
+      
+      <?php
+      break;
+
+    default:
+      ?>
+
+        <h1>The MAFEX® ULV Fine Spray Unit - for application of liquid products</h1>
+        <a href="<?php echo PICTURES_LOCATION; ?>mafex_atomizer_benefit.jpg" rel="lightbox[mafexbenefits]" title="The Mafex atomizer makes 30 million droplets from every ml. of liquid."><img class="pright" src="<?php echo THUMBS_LOCATION; ?>mafex_atomizer_benefit.jpg" alt=""></a>
+        <p>The MAFEX Potato and Fruit fine spray unit is based on ULV (Ultra-Low Volume) technology, through which the required output volume can be reduced significantly thanks to optimal dispersion of active ingredients with uniform size of droplets.  It has been used successfully around the world since 1976, in particular for the optimal protection of potato harvest yield with sprout inhibitors and fungicides, as well as for fruits with wax and fungicides.</p>
+
+        <h2>How MAFEX® works</h2>
+        <a href="<?php echo PICTURES_LOCATION; ?>mafex_portable_benefit.jpg" rel="lightbox[mafexbenefits]" title="The MAFEX® is highly portable and can be moved where needed in your operation."><img class="pleft" src="<?php echo THUMBS_LOCATION; ?>mafex_portable_benefit.jpg" alt=""></a>  
+        <p>The complete coverage of the nodular surface is indispensable for fighting pests. Individual active agent particles must cover the surface with the least possible separation gap. In order to achieve an equal distribution, the chemical agent available must be dispersed in a multitude of uniformly sized droplets. </p>
+        <p>On the MAFEX® unit, this is accomplished by means of centrifugal force using a specially-developed rotation atomizer that generates approx. 30 million droplets from 1 ml of liquid (approx. 900 million droplets from one ounce). The droplets are uniformly deposited on the surface through the vertical air steam generated by the rotation disc and gravity, as well as by the rotating and falling motion of the potatoes or fruit.</p>
+        <p>Automatic priming of the system, meaning a check of all functions and the conveyance of liquid to the atomizer(s) prior to the start of application, is made possible by a modern control unit. Desired volumes can be set directly on the control unit. During operation, atomizer function is monitored and an alarm is triggered in the event of a malfunction. A flow sensor is also available as an option, which will let you know the second that liquid is not being delivered to the atomizer for any reason.</p>
+        <br />
+
+      <?php
+      break;
+  }
+
+?>
+
+  <div id="homeProducts">
+
+  <div class="productTypeListWrapper">
+    <?php foreach ($mankarMain->productTypes as $type)
+    { 
+        ?>
+        <div class="productTypeWrapper">
+          <div class="productType">
+
+            <div class="productImage">
+              <?php
+              echo '<a href="'.$type['pretty_url'].'" title="'.$type['name'].'">';
+              echo '<img src="'.THUMBS_LOCATION.$type['thumbnail'].'" alt="'.$type['name'].'">';
+              echo '</a>';
+            ?>  
+            </div>
+            <div class="productName">
+
+              <a class="navLink" href="<?php echo $type['pretty_url']; ?>"><?php echo $type['name']; ?></a>
+
+            </div>
+          </div>
+        </div>
+
+    <?php 
+    } 
+    ?>
+  </div>
+  <br class="clear" />
+  </div>
+</div>
