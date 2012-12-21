3080fe4dca9601ee481b05b181d000b64aac9bbd
Nick <selling_tokyo@yahoo.com>
2012-12-21 15:45:39 -0500

    updated footer

--- www/views/footer.php
+++ www/views/footer.php
@@ -2,6 +2,7 @@
 <div id="container2">
     <div class="divBottom">
     	<p><i>When using any chemicals please follow label instructions and applicable local guidelines.</i><br>
+    	Mankar®, Mafex®, Rofa®, and Mantis ULV® are registered trademarks of Mantis ULV-Sprühgeräte GmbH in Germany.<br>	
         Copyright &copy;<?php echo date('Y'); ?> Mankar Distributing Inc. &nbsp;&nbsp;All Rights Reserved.<br>
         phone: 647-309-7826&nbsp;&nbsp;&nbsp;fax: 888-510-2688&nbsp;&nbsp;&nbsp;<a href="mailto:&#105;&#110;&#102;&#111;&#064;&#109;&#097;&#110;&#107;&#097;&#114;&#046;&#099;&#097;">&#105;&#110;&#102;&#111;&#064;&#109;&#097;&#110;&#107;&#097;&#114;&#046;&#099;&#097;</a>
         </p>
